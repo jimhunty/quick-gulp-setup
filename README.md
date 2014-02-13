bfd72d2c41e461ad07c0da4b6129d8c049a98ae0
James Miller <james@james-miller.co.uk>
2014-02-13 20:46:40 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Quick Gulp Setup
+
+I wanted to try out Gulp in comparison to Grunt. I generally like to build my site using SASS with Compass. Susy is my grid system of choice. I really like the speed of development Live Reload gives you. So i replicated it and it actually runs a lot faster. 
+
+Im so pleased, I thought I would share it.
+
+## Requirements
+* Node JS and Ruby
+* SASS, Compass and Susy gems
+* Gulp
+* Live Reload Chrome plugin
+
+## Usage
+* CD to the directory in your terminal
+* ```npm install```
+* ```gulp```
+
+## Improvements
+* Environments for test and production
+
+## Problems?
+
+Tweet me [@jimhunty](https://twitter.com/jimhunty)
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var express = require('express');
+var path = require('path');
+var tinylr = require('tiny-lr');
+var compass = require('gulp-compass');
+ 
+var createServers = function(port, lrport) {
+  var lr = tinylr();
+  lr.listen(lrport, function() {
+    gutil.log('LR Listening on', lrport);
+  });
+ 
+  var app = express();
+  app.use(express.static(path.resolve('./')));
+  app.listen(port, function() {
+    gutil.log('Listening on', port);
+  });
+ 
+  return {
+    lr: lr,
+    app: app
+  };
+};
+
+var servers = createServers(8080, 35729);
+
+gulp.task('compass', function() {
+    gulp.src('./sass/*.scss')
+        .pipe(compass({
+            css: 'css',
+            image: 'img',
+            require: ['susy']
+        }))
+        .pipe(gulp.dest('temp'));
+});
+ 
+gulp.task('default', function(){
+	gulp.run('compass');
+  gulp.watch(["./**/*", "!./node_modules/**/*"], function(evt){
+    gutil.log(gutil.colors.cyan(evt.path), 'changed');
+    servers.lr.changed({
+      body: {
+        files: [evt.path]
+      }
+    });
+  });
+});
